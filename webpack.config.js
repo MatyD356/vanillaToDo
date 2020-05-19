c4162cf997637d8e3d8b33c94577fce262f81c66
Mateusz <matyd356@gmail.com>
2020-05-19 13:37:24 +0200

    finish project setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,13 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: './src/index.js',
+    entry: ['./src/index.js', 'style.scss'],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+        ]
     }
 };
